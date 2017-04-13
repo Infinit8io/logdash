d4791c68783e8a775d057fabc3b170cb37602e1a
Dom <ducommun.dom@gmail.com>
2017-04-13 13:51:12 +0200

    ret & example_output test

--- fetch_metrics/psutil_fetching.py
+++ fetch_metrics/psutil_fetching.py
@@ -116,8 +116,8 @@ def psutil_fetching():
         },
     }
 
-    print(json.dumps(log, indent=4))
-    #return log
+    #print(json.dumps(log, indent=4))
+    return log
 
 if __name__ == "__main__":
-    psutil_fetching()
+    print(json.dumps(psutil_fetching(), indent=4))
